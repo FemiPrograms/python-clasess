b66685cab8766d819530dfc8a0b5f93c880ce171
FemiPrograms <femistar21@gmail.com>
2025-06-27 19:45:23 +0100

    Add files via upload

---
+++ Class1.py
@@ -0,0 +1,11 @@
+'''
+print('Hello\nWorld', 2+3) #activity 1
+#activity 2
+print('Step 1 take a variable x = 1')
+x = 1
+print('Step 2 take another variable y = 3')
+y = 3
+print('Step 3 add x+y')
+print(x+y)
+print('Done')
+'''

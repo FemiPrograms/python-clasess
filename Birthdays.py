6482d404dad9db79d04baf16d144c2510287a6fa
FemiPrograms <femistar21@gmail.com>
2025-08-20 14:07:28 +0100

    Add files via upload

---
+++ Birthdays.py
@@ -0,0 +1,11 @@
+john = '01/01/2001'
+bob = '02/02/2002'
+isaac = '03/03/2003'
+charlie = '04/04/2004'
+cliff = '05/05/2005'
+print ('''Here are ther birthdays Raj:
+      John:''', john, '''
+      Bob:''', bob, '''
+      Isaac:''', isaac, '''
+      Charlie:''', charlie, '''
+      Cliff:''' , cliff)
